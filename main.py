0ecdbbd6bb7d6bb4323ade184a3ca86ce653220b
Tyzinkowy <marcin.wcale.nie@gmail.com>
2022-04-23 23:36:31 +0200

    player is able to play one round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from mimetypes import init
-from random import Random, random
-#trzeba gdzieś dodać beat - wysolość zakładu
-
+import random
+#trzeba gdzieś dodać beat - wysokość zakładu
 
 def creat_deck():
     deck = []
@@ -12,6 +11,18 @@ def creat_deck():
         for card in cards:
             deck.append(Card(suit, card, cards_values[card]))
     return deck
+
+def player_turn():
+    player_choice = input("What is your next move?(draw, split, pass) ")
+    if player_choice == "draw":
+        player.draw(deck)
+    elif player_choice == "split":
+        player.split()
+    elif player_choice == "pass":
+        player.end_turn(casino, deck)
+    else:
+        player_turn()
+
 
 class Card:
     def __init__(self, suit,value, card_value):
@@ -27,8 +38,23 @@ class User:
         self.cards = []
         self.beat = 0
 
-    def draw():
-        return 0
+    def set_beat(self):
+        beat = input("You have {chips}. How much do you wont beat? ".format(chips = self.chips))
+        if int(beat) <= 0 and int(beat) > self.chips:
+            print("something")
+        else:
+            self.beat = int(beat)
+            print("You beat {beat} chips".format(beat = self.beat))
+
+
+    def draw(self, deck):
+        card = random.choice(deck)
+        self.cards.append(card)
+        deck.remove(card)
+        print("your cards are: ")
+        for card in self.cards:
+            print("{suit} {value}".format(suit = card.suit, value = card.value))
+        player_turn()
 
     def split():
         return 0
@@ -36,8 +62,11 @@ class User:
     def double_down():
         return 0
 
-    def end_turn():
-        return 0
+    def end_turn(self, casino, deck):
+        #place holder pewnie bedzie wywołąnie metody z kasyna
+        print("Now it's Casino turn.")
+        casino.check_heand(self, deck)
+
 
 class Casino:
     def __init__(self):
@@ -46,15 +75,15 @@ class Casino:
 
     def dael_the_cards(self, player, deck):
         while len(player.cards) < 2:
-            dael = Random.choice(deck)
+            dael = random.choice(deck)
             player.cards.append(dael)
             deck.remove(dael)
-            print("Your cards are {suit}{value}".format(suit = dael.suit, value = dael.value))
+            print("Your cards are {suit} {value}".format(suit = dael.suit, value = dael.value))
         while len(self.heand) < 2:
-            dael = Random.choice(deck)
+            dael = random.choice(deck)
             self.heand.append(dael)
             deck.remove(dael)
-        print("Casino first card is {suit}{value}".format(suit = self.heand[0].suit, value = self.heand[0].value))
+        print("Casino first card is {suit} {value}".format(suit = self.heand[0].suit, value = self.heand[0].value))
         
         
     def win(self, player):
@@ -68,27 +97,38 @@ class Casino:
         print("You win. You get {beat} chips, and you have now {chips} chips.".format(beat = player.beat, chips = player.chips))
 
 
-    def check_heand(self, player):
-        #system sprawdzania punktów na dłoni pracza i casyna
+    def check_heand(self, player, deck):
+        print("Casino second card is {suit} {value}".format(suit = self.heand[1].suit, value = self.heand[1].value))
         player_score = 0
         casino_score = 0
-        #for card in player.cards:
 
+        for card in player.cards:
+            player_score += card.card_value
+        
+        for card in self.heand:
+            casino_score += card.card_value
+        
+        while (player_score > casino_score):
+            print("Casino draw a card.")
+            card = random.choice(deck)
+            self.heand.append(card)
+            deck.remove(card)
+            casino_score += card.card_value
+            print("New card is {suit} {value}".format(suit = card.suit, value = card.value))
 
-        if self.heand <= 21 and self.heand > player.heand:
+        if casino_score <= 21 and casino_score > player_score:
             self.win(player)
-        elif self.heand > 21 or self.heand < player.heand:
+        elif casino_score > 21 or casino_score < player_score:
             self.lose(player)
         else:
             print("There is a tie. You still have {chips} chips.".format(chips = player.chips))
 
 
-
-
-
-    
 username = input("What is your name? ")
 player = User(username)
 print("Welcom "+ username +" in Blackjak game, also know as 21.")
 casino = Casino()
-casino.dael_the_cards(player, creat_deck())
+deck = creat_deck()
+player.set_beat()
+casino.dael_the_cards(player, deck)
+player_turn()
